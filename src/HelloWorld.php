8ce9c829f5b7431dc51405b04bfcf4a4dbe72e38
ShawnWang <kingshark8848@gmail.com>
2018-12-05 11:38:55 +1100

    init commit

---
+++ src/HelloWorld.php
@@ -0,0 +1,20 @@
+<?php namespace Kingshark8848\LogixCrmSdkPhp;
+
+/**
+ *  A sample class
+ *
+ * @author Shawn Wang
+ */
+class HelloWorld
+{
+
+
+    /**
+     * Sample method
+     * @return string
+     */
+    public function method1()
+    {
+        return "Hello World";
+    }
+}
